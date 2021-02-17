d819d86cf3a70585f8ceec12163f5889d6e7623d
Danny Tomkins <tomkinsdan@gmail.com>
2021-02-16 23:18:49 -0600

    initial deploy to heroku

---
+++ public/db.js
@@ -0,0 +1,9 @@
+// variable based on browser being used
+const indexedDB =
+    //window.indexedDB ||
+    //etc
+
+// db variable
+
+
+const request = indexedDB.open("budget", 1);
---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+console.log("Hi. Service worker here")
+
+//declare cache for logic and js
+
+
+//declare cache for data
+
+
