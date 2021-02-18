78fe27a1b26471f5aae4d298ba5fea2b84ce262b
Danny Tomkins <tomkinsdan@gmail.com>
2021-02-17 20:45:44 -0600

    added install function on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 console.log("Hi. Service worker here")
 
 //define cache for logic and js
-const CACHE = "budget-logic-cache"
+const BUDGET_CACHE = "budget-logic-cache"
 
 //define cache for data
-const DATA_CACHE = "budget-data-cache"
+const BUDGET_DATA_CACHE = "budget-data-cache"
 
 var urlCacheArray = [
     "/",
@@ -15,3 +15,12 @@ var urlCacheArray = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ]
+
+//install function
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(BUDGET_CACHE).then(function (cache) {
+            return (cache.addAll(urlCacheArray))
+        })
+    );
+});
